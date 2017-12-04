7effb7de9c421bf010d86e70471e781200ca8231
Kara <kara17@ru.is>
2017-12-04 14:08:53 +0000

    update

--- PIZZA/GLOBAL/GlobalHelp.cpp
+++ PIZZA/GLOBAL/GlobalHelp.cpp
@@ -13,4 +13,5 @@ void GlobalHelp::displayLogo(){
     cout << " | |   | |/ / / / (_| | | .  | | || (__| | | |  __/ | | | " << endl;
     cout << " |_|   |_/___/___|__,_| |_||_|_|___|___|_| |_||___|_| |_| " << endl;
     cout << endl << endl;
+    cout << "test kara" << endl;
 }
