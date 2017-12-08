80abcfc2c1a4425aae8c042e4bbd005eaf831757
asdiseinars <asdis@eythor.is>
2017-12-08 12:07:19 +0000

    Lagfræingar á locations

--- PIZZA/CONTROLLERS/BakerController.cpp
+++ PIZZA/CONTROLLERS/BakerController.cpp
@@ -7,42 +7,32 @@ BakerController::BakerController() {
 void BakerController::init() {
     displayLogo();
     displayBakerLogo();
-    displayBakerUI();
-
-    char selection = '\0';
-    cin >> selection;
-
     chooseLocation();
 
-    if(selection == '1') {
-        clearScreen();
-        displayLogo();
-        displayBakerLogo();
-        displayBakerOrdersUI();
-    }
+    /*char selection = '\0';
+    cin >> selection;
 
     else if (selection == 'b') {
         HomeController home;
         home.init();
-    }
+    }*/
 }
 
 void BakerController::chooseLocation(){
     vector<Location> locations;
-    LocationData locationData;
     locations = locationData.retrieveAllLocations();
 
     cout << "LOCATIONS" << endl;
-    cout << "------------------------------------------" << endl << endl;
+    cout << "------------------------------------------" << endl;
     for(unsigned int i = 0; i < locations.size(); i++){
         cout << "Location number: [" << i+1 << "]" << endl;
-        cout << locations[i] << endl << endl;
+        cout << locations[i] << endl;
     }
     cout << "------------------------------------------" << endl;
-
-    //cout << "Choose location: ";
+    cout << "Choose location: ";
     char input = 0;
     cin >> input;
+
     int inputInt = input - 48;
     for(unsigned int i = 0; i < locations.size(); i++){
         if(inputInt == i + 1){
